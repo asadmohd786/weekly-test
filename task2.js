c4aa8b8b8e49a5828e0a56d8b522033d00982740
asadmohd786 <92883306+asadmohd786@users.noreply.github.com>
2023-05-28 22:42:17 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,19 @@
+let person = {
+    name: "Asad",
+    age: 25,
+    address: "123 Main Street"
+  };
+  
+  console.log(person.name);    
+  console.log(person.age);     
+  console.log(person.address); 
+  
+  person.age = 30;
+  console.log(person.age);   
+  
+  person.email = "asmohd7867@gmail.com";
+  console.log(person.email);   
+  
+  delete person.address;
+  console.log(person.address); 
+  
