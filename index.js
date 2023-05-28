c6b6b4fa664501b7faaa4f67f284cccb5834ecd3
asadmohd786 <92883306+asadmohd786@users.noreply.github.com>
2023-05-28 21:41:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+let fruits = ["apple", "banana", "orange", "grape"];
 
+fruits.push("kiwi");
+console.log(fruits); 
+
+fruits.pop();
+console.log(fruits);
+
+fruits.unshift("lemon");
+console.log(fruits); 
+
+fruits.shift();
+console.log(fruits); 
+
+let index = fruits.indexOf("orange");
+console.log(index); 
+
+let citrus = fruits.slice(1, 3);
+console.log(citrus); 
